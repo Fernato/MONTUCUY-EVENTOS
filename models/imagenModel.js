1b5212eeae8647628b47e604565e4b32c9a8e38c
FernatoDiaz <fer12329@gmail.com>
2022-05-31 20:25:43 -0500

    prueba

--- models/imagenModel.js
+++ models/imagenModel.js
@@ -37,7 +37,7 @@ imagenSchema.methods.setImagen = function setImagen (filename) {
     const port = process.env.PORT || 5000;
    // this.url = `http://localhost:${port}/${filename}`
    // this.url = `${host}:${port}/${filename}`
-    this.url = `${host}:public/${filename}`
+    this.url = `${host}/public/${filename}`
    // this.url = `https://eventos-montucuy.herokuapp.com/${filename}`
 
 }
